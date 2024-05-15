2f398242aa79c2bd5298a11e62c497b19978ff0a
unknown <saurabhpasi.sp@gmail.com>
2024-05-15 10:46:57 +0530

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { Footer, Hero, Navbar, Shop, Work } from './container'
+
+const app = () => {
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <Work/>
+      <Shop/>
+      <Footer/>
+    </>
+  )
+}
+
+export default app
---
+++ src/container/index.js
@@ -0,0 +1,13 @@
+import Footer from './footer/Footer'
+import Hero from './hero/Hero'
+import Navbar from './navbar/Navbar'
+import Shop from './shop/Shop'
+import Work from './work/Work'
+
+export {
+    Footer,
+    Hero,
+    Navbar,
+    Shop,
+    Work,
+}
